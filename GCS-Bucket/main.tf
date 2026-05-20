0582466daf440cfe837faa2093fb504fef1b27ed
vartika786 <vartikasrivastava234@gmail.com>
2026-05-20 15:25:26 +0530

    Added GCS Bucket Terraform project

---
+++ GCS-Bucket/main.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket" "bucket" {
+  name          = var.bucket_name
+  location      = "US"
+  force_destroy = true
+}
---
+++ GCS-Bucket/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  default = "project-4ccde3e9-1daa-4874-b49"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "bucket_name" {
+  default = "vartika-gcs-bucket-786"
+}
